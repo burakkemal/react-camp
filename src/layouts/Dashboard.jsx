b5dcca09d8949e3e2abdfbc243d0027ab3a385b0
Burak Kemal Koyuncu <77545526+burakkemal@users.noreply.github.com>
2021-06-06 01:45:55 +0300

    ProductList added

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { Button, Icon, Label } from "semantic-ui-react";
+
 import ProductList from "../pages/ProductList";
 import Category from "./Category";
 import Navi from "./Navi";
+import { Grid, GridColumn, GridRow } from 'semantic-ui-react'
 export default function Dashboard() {
   return (
     <div>
-      <Navi></Navi>
-      <Category/>
-      <ProductList/>
+      <Grid>
+        <GridRow>
+          <GridColumn width={4}>
+             <Category/>
+          </GridColumn>
+          <GridColumn width={12}>
+             <ProductList/>
+          </GridColumn>
+        </GridRow>
+      </Grid>
+     
+     
     </div>
   );
 }
