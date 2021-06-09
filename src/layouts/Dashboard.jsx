9ef46e8e2557096fd2837bfc13df74246b53a26e
Burak Kemal Koyuncu <77545526+burakkemal@users.noreply.github.com>
2021-06-09 23:34:53 +0300

    Route added

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -4,21 +4,27 @@ import ProductList from "../pages/ProductList";
 import Category from "./Category";
 import Navi from "./Navi";
 import { Grid, GridColumn, GridRow } from 'semantic-ui-react'
+import { Route } from "react-router";
+import ProductDetail from "../pages/ProductDetail";
+import CartDetail from "../pages/CartDetail";
 export default function Dashboard() {
   return (
     <div>
       <Grid>
         <GridRow>
           <GridColumn width={4}>
-             <Category/>
+            <Category />
           </GridColumn>
           <GridColumn width={12}>
-             <ProductList/>
+            <Route exact path="/" component={ProductList} />
+            <Route exact path="/products" component={ProductList} />
+            <Route exact path="/cart" component={CartDetail} />
+            <Route exact path="/products/:id" component={ProductDetail} />
           </GridColumn>
         </GridRow>
       </Grid>
-     
-     
+
+
     </div>
   );
 }
